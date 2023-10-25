62a953da75ad7622914246009c1f17ec95941391
KingdomCoder <kingdomcoding@gmail.com>
2023-10-25 11:25:25 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ mix ecto.setup
 mix event_store.setup
 
 # Make server file executable
-chmod a+x _build/prod/rel/soon_ready/bin/server
+chmod a+x _build/prod/rel/soon_ready/bin/soon_ready
 
 # Start server
-_build/prod/rel/soon_ready/bin/server
+_build/prod/rel/soon_ready/bin/soon_ready
