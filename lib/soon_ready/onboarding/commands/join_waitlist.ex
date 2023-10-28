b1feb040042dfbef2266b4ca6ceadd0d059f9aaf
KingdomCoder <kingdomcoding@gmail.com>
2023-10-28 20:39:36 +0100

    Encryption details generation put in transaction

--- lib/soon_ready/onboarding/commands/join_waitlist.ex
+++ lib/soon_ready/onboarding/commands/join_waitlist.ex
@@ -4,7 +4,6 @@ defmodule SoonReady.Onboarding.Commands.JoinWaitlist do
   alias __MODULE__.Validations.EmailIsUnique
   alias SoonReady.Onboarding.DomainConcepts.EmailAddress
   alias SoonReady.Onboarding.DomainEvents.WaitlistJoined
-  alias SoonReady.Onboarding.PersonallyIdentifiableInformation.EncryptionDetails
 
   attributes do
     uuid_primary_key :id
@@ -20,18 +19,7 @@ defmodule SoonReady.Onboarding.Commands.JoinWaitlist do
     define :create
   end
 
-  defp encrypt(plain_text, for: person_id) do
-    with :error <- SoonReady.Vault.encrypt(%{person_id: person_id, plain_text: plain_text}, :onboarding) do
-      {:error, :encryption_failed}
-    end
-  end
-
   def execute(%{__struct__: __MODULE__, id: id, email: email} = command, aggregate_state) do
-    # TODO: Put in transaction
-    with {:ok, _encryption_details} <- EncryptionDetails.generate(%{person_id: id}),
-          {:ok, email_hash} <- encrypt(email, for: id)
-    do
-      WaitlistJoined.create!(%{id: id, email_hash: email_hash})
-    end
+    WaitlistJoined.create!(%{id: id, email: email})
   end
 end
--- lib/soon_ready/onboarding/domain_events/waitlist_joined.ex
+++ lib/soon_ready/onboarding/domain_events/waitlist_joined.ex
@@ -2,10 +2,42 @@ defmodule SoonReady.Onboarding.DomainEvents.WaitlistJoined do
   use Ash.Resource, data_layer: :embedded,
     extensions: [SoonReady.Ash.Extensions.JsonEncoder]
 
+  alias SoonReady.Onboarding.DomainConcepts.EmailAddress
+  alias SoonReady.Onboarding.PersonallyIdentifiableInformation.EncryptionDetails
+
   attributes do
     attribute :id, :uuid, allow_nil?: false, primary_key?: true
-    attribute :email_hash, :string, allow_nil?: false
+    attribute :email_hash, :string
     attribute :event_version, :integer, default: 1
+  end
+
+  defp hash(plain_text, for: person_id) do
+    with :error <- SoonReady.Vault.encrypt(%{person_id: person_id, plain_text: plain_text}, :onboarding) do
+      {:error, "encryption_failed"}
+    end
+  end
+
+
+  actions do
+    create :create do
+      argument :email, EmailAddress, allow_nil?: false
+
+      change fn changeset, _context ->
+        Ash.Changeset.before_action(changeset, fn changeset ->
+          person_id = Ash.Changeset.get_attribute(changeset, :id)
+          email = Ash.Changeset.get_argument(changeset, :email)
+
+          with {:ok, _encryption_details} <- EncryptionDetails.generate(%{person_id: person_id}),
+                {:ok, email_hash} <- hash(email, for: person_id)
+          do
+            Ash.Changeset.change_attribute(changeset, :email_hash, email_hash)
+          else
+            {:error, error} ->
+              Ash.Changeset.add_error(changeset, error)
+          end
+        end)
+      end
+    end
   end
 
   code_interface do
