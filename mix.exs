0389b95de9ac927a744bc016dbbfaa8d1d385d8c
KingdomCoder <kingdomcoding@gmail.com>
2023-12-17 22:12:54 +0100

    Tests reorganized

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule SoonReady.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
+      test_paths: ["lib"],
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
