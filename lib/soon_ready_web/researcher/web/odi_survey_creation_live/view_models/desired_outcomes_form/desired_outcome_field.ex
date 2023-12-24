4a3bae400f3dee464d2073a7de8989615903657a
KingdomCoder <kingdomcoding@gmail.com>
2023-12-24 22:47:08 +0100

    Domain data types used in interface layer

--- lib/soon_ready_web/researcher/web/odi_survey_creation_live/view_models/desired_outcomes_form/desired_outcome_field.ex
+++ lib/soon_ready_web/researcher/web/odi_survey_creation_live/view_models/desired_outcomes_form/desired_outcome_field.ex
@@ -1,9 +1,9 @@
 defmodule SoonReadyWeb.Researcher.Web.OdiSurveyCreationLive.ViewModels.DesiredOutcomesForm.DesiredOutcomeField do
   use Ash.Resource, data_layer: :embedded
 
-  alias SoonReadyWeb.Researcher.Web.OdiSurveyCreationLive.ViewModels.DesiredOutcomesForm.DesiredOutcomeConcept
+  alias SoonReady.SurveyManagement.DomainDataTypes.DesiredOutcome
 
   attributes do
-    attribute :value, DesiredOutcomeConcept, allow_nil?: false
+    attribute :value, DesiredOutcome, allow_nil?: false
   end
 end
--- lib/soon_ready_web/researcher/web/odi_survey_creation_live/view_models/desired_outcomes_form/job_step_concept.ex
+++
@@ -1,4 +0,0 @@
-defmodule SoonReadyWeb.Researcher.Web.OdiSurveyCreationLive.ViewModels.DesiredOutcomesForm.JobStepConcept do
-  # TODO: Change to add constraints to ensure that the value matches the expected format
-  use Ash.Type.NewType, subtype_of: :string
-end
