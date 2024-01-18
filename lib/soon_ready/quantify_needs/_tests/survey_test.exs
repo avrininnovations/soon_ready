f5ea7bb4c85b812c05dd4b61fd150a88da4fb585
KingdomCoder <kingdomcoding@gmail.com>
2024-01-18 12:31:18 +0100

    Update survey_test.exs

--- lib/soon_ready/quantify_needs/_tests/survey_test.exs
+++ lib/soon_ready/quantify_needs/_tests/survey_test.exs
@@ -43,39 +43,41 @@ defmodule SoonReady.QuantifyNeeds.Tests.Survey do
     ]
   }
 
-  test "WHEN: A researcher tries to create a survey, THEN: A survey is created" do
-    # TODO: Test the fact that the actor is an Avrin researcher
+  describe "Happy Path" do
+    test "WHEN: A researcher tries to create a survey, THEN: A survey is created" do
+      # TODO: Test the fact that the actor is an Avrin researcher
 
-    case Survey.create(@survey_details) do
-      {:ok, %{id: survey_id} = _command} ->
-        assert_receive_event(Application, SurveyCreated,
-          fn event -> event.id == survey_id end,
-          fn event ->
-            assert SoonReady.Utils.is_equal_or_subset?(event.brand, @survey_details.brand)
-            assert SoonReady.Utils.is_equal_or_subset?(event.market, @survey_details.market)
-            assert SoonReady.Utils.is_equal_or_subset?(event.job_steps, @survey_details.job_steps)
-            assert SoonReady.Utils.is_equal_or_subset?(event.screening_questions, @survey_details.screening_questions)
-            assert SoonReady.Utils.is_equal_or_subset?(event.demographic_questions, @survey_details.demographic_questions)
-            assert SoonReady.Utils.is_equal_or_subset?(event.context_questions, @survey_details.context_questions)
-          end
-        )
-      {:error, error} ->
-        flunk("Expected survey to be created but got: #{inspect(error)}")
-    end
-  end
-
-  test "GIVEN: A survey has been created, WHEN: A researcher tries to publish the survey, THEN: The survey is published" do
-    # TODO: Test the fact that the actor is an Avrin researcher
-    
-    with {:ok, %{id: survey_id} = survey} <- Survey.create(@survey_details) do
-      case Survey.publish(survey) do
-        {:ok, %{id: ^survey_id}} ->
-          assert_receive_event(Application, SurveyPublished,
+      case Survey.create(@survey_details) do
+        {:ok, %{id: survey_id} = _command} ->
+          assert_receive_event(Application, SurveyCreated,
             fn event -> event.id == survey_id end,
-            fn _event ->:ok end
+            fn event ->
+              assert SoonReady.Utils.is_equal_or_subset?(event.brand, @survey_details.brand)
+              assert SoonReady.Utils.is_equal_or_subset?(event.market, @survey_details.market)
+              assert SoonReady.Utils.is_equal_or_subset?(event.job_steps, @survey_details.job_steps)
+              assert SoonReady.Utils.is_equal_or_subset?(event.screening_questions, @survey_details.screening_questions)
+              assert SoonReady.Utils.is_equal_or_subset?(event.demographic_questions, @survey_details.demographic_questions)
+              assert SoonReady.Utils.is_equal_or_subset?(event.context_questions, @survey_details.context_questions)
+            end
           )
         {:error, error} ->
-          flunk("Expected survey to be published but got: #{inspect(error)}")
+          flunk("Expected survey to be created but got: #{inspect(error)}")
+      end
+    end
+
+    test "GIVEN: A survey has been created, WHEN: A researcher tries to publish the survey, THEN: The survey is published" do
+      # TODO: Test the fact that the actor is an Avrin researcher
+
+      with {:ok, %{id: survey_id} = survey} <- Survey.create(@survey_details) do
+        case Survey.publish(survey) do
+          {:ok, %{id: ^survey_id}} ->
+            assert_receive_event(Application, SurveyPublished,
+              fn event -> event.id == survey_id end,
+              fn _event ->:ok end
+            )
+          {:error, error} ->
+            flunk("Expected survey to be published but got: #{inspect(error)}")
+        end
       end
     end
   end
