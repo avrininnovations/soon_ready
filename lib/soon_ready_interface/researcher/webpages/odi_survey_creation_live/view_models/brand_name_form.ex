6cd3196817fef9f6fd2a117abb8a3f6218dfb433
KingdomCoder <kingdomcoding@gmail.com>
2024-01-23 16:10:05 +0100

    View models deleted

--- lib/soon_ready_interface/researcher/webpages/odi_survey_creation_live/view_models/brand_name_form.ex
+++
@@ -1,7 +0,0 @@
-# defmodule SoonReadyInterface.Researcher.Webpages.OdiSurveyCreationLive.ViewModels.BrandNameForm do
-#   use Ash.Resource, data_layer: :embedded
-
-#   attributes do
-#     attribute :brand_name, :string, allow_nil?: false
-#   end
-# end
--- lib/soon_ready_interface/researcher/webpages/odi_survey_creation_live/view_models/desired_outcomes_form/desired_outcome_field.ex
+++
@@ -1,9 +0,0 @@
-defmodule SoonReadyInterface.Researcher.Webpages.OdiSurveyCreationLive.ViewModels.DesiredOutcomesForm.DesiredOutcomeField do
-  use Ash.Resource, data_layer: :embedded
-
-  alias SoonReady.QuantifyingNeeds.Survey.DomainDataTypes.DesiredOutcome
-
-  attributes do
-    attribute :value, DesiredOutcome, allow_nil?: false
-  end
-end
