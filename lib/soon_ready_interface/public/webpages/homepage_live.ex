e170258c55ed470bf41ecc03b48fac813e671a04
KingdomCoder <kingdomcoding@gmail.com>
2024-02-12 08:23:16 +0100

    improvement: marketing phrases

--- lib/soon_ready_interface/public/webpages/homepage_live.ex
+++ lib/soon_ready_interface/public/webpages/homepage_live.ex
@@ -30,10 +30,10 @@ defmodule SoonReadyInterface.Public.Webpages.HomepageLive do
       <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-12 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div class="mr-auto place-self-center lg:col-span-7 xl:col-span-8">
           <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-            Launching the future, one software company at a time...
+            Building the future, one product launch at a time...
           </h1>
           <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-            SoonReady is home for innovators who are on the journey to launch successful software companies. Join other innovators on our waitlist.
+            SoonReady is home for innovators who are on the journey to launch successful products and services. Join other innovators on our waitlist.
           </p>
           <%= render_slot(@waitlist_form) %>
         </div>
