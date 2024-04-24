8afb0421ef5f85d2f89f0b16d7ca7e6c5e46ea1c
KingdomCoder <kingdomcoding@gmail.com>
2024-04-24 20:26:57 +0100

    improvement: pii hashing logic added

--- lib/soon_ready/user_authentication/_api.ex
+++ lib/soon_ready/user_authentication/_api.ex
@@ -1,3 +1,7 @@
 defmodule SoonReady.UserAuthentication.Api do
   use Ash.Api
+
+  resources do
+    resource SoonReady.UserAuthentication.Encryption.EncryptionKey
+  end
 end
---
+++ lib/soon_ready/user_authentication/encryption/encryption_key.ex
@@ -0,0 +1,45 @@
+defmodule SoonReady.UserAuthentication.Encryption.EncryptionKey do
+  # TODO: Update to postgres
+  # use Ash.Resource, data_layer: AshPostgres.DataLayer
+  use Ash.Resource, data_layer: Ash.DataLayer.Ets
+
+  require Logger
+
+  attributes do
+    attribute :user_id, :uuid, allow_nil?: false, primary_key?: true
+    attribute :encoded_key, :string, allow_nil?: false, private?: true
+  end
+
+  calculations do
+    calculate :key, :string, fn record, _context ->
+      {:ok, Base.decode64!(record.encoded_key)}
+    end
+  end
+
+  changes do
+    change load(:key)
+  end
+
+  preparations do
+    prepare fn query, _context ->
+      Ash.Query.load(query, [:key])
+    end
+  end
+
+  actions do
+    defaults [:read]
+
+    create :generate do
+      primary? true
+
+      change fn changeset, _context ->
+        Ash.Changeset.change_attribute(changeset, :encoded_key, 32 |> :crypto.strong_rand_bytes() |> Base.encode64())
+      end
+    end
+  end
+
+  code_interface do
+    define_for SoonReady.UserAuthentication.Api
+    define :generate
+  end
+end
