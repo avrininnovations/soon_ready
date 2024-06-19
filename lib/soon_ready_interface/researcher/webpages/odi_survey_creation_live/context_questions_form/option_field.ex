25098d78e4f4c48c5d317525b4d940e1d0fa8a18
KingdomCoder <kingdomcoding@gmail.com>
2024-06-19 12:07:44 +0100

    chore: uncomment context question field modules

--- lib/soon_ready_interface/researcher/webpages/odi_survey_creation_live/context_questions_form/option_field.ex
+++ lib/soon_ready_interface/researcher/webpages/odi_survey_creation_live/context_questions_form/option_field.ex
@@ -1,7 +1,7 @@
-# defmodule SoonReadyInterface.Researcher.Webpages.OdiSurveyCreationLive.ContextQuestionsForm.OptionField do
-#   use Ash.Resource, data_layer: :embedded
+defmodule SoonReadyInterface.Researcher.Webpages.OdiSurveyCreationLive.ContextQuestionsForm.OptionField do
+  use Ash.Resource, data_layer: :embedded
 
-#   attributes do
-#     attribute :value, :string, allow_nil?: false
-#   end
-# end
+  attributes do
+    attribute :value, :string, allow_nil?: false, public?: true
+  end
+end
