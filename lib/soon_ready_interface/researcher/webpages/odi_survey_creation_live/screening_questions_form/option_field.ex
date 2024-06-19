0ee7a82526a6f5ff101082e5c49fed9df1c9119b
KingdomCoder <kingdomcoding@gmail.com>
2024-06-19 10:47:25 +0100

    chore: uncomment screening question page modules

--- lib/soon_ready_interface/researcher/webpages/odi_survey_creation_live/screening_questions_form/option_field.ex
+++ lib/soon_ready_interface/researcher/webpages/odi_survey_creation_live/screening_questions_form/option_field.ex
@@ -1,8 +1,8 @@
-# defmodule SoonReadyInterface.Researcher.Webpages.OdiSurveyCreationLive.ScreeningQuestionsForm.OptionField do
-#   use Ash.Resource, data_layer: :embedded
+defmodule SoonReadyInterface.Researcher.Webpages.OdiSurveyCreationLive.ScreeningQuestionsForm.OptionField do
+  use Ash.Resource, data_layer: :embedded
 
-#   attributes do
-#     attribute :value, :string, allow_nil?: false
-#     attribute :is_correct_option, :boolean, allow_nil?: false
-#   end
-# end
+  attributes do
+    attribute :value, :string, allow_nil?: false, public?: true
+    attribute :is_correct_option, :boolean, allow_nil?: false, public?: true
+  end
+end
--- lib/soon_ready_interface/researcher/webpages/odi_survey_creation_live_test.exs
+++ lib/soon_ready_interface/researcher/webpages/odi_survey_creation_live_test.exs
@@ -269,53 +269,53 @@ defmodule SoonReadyInterface.Researcher.Webpages.OdiSurveyCreationLiveTest do
     end
   end
 
-  # describe "Desired Outcomes" do
-  #   test "GIVEN: Forms in previous pages have been filled, WHEN: Researcher tries to add two job steps, THEN: Two job step fields should be on the page", %{conn: conn} do
-  #     {:ok, view, _html} = live(conn, ~p"/odi-survey/create")
-  #     submit_landing_page_form(view)
-  #     submit_market_definition_form(view)
+  describe "Desired Outcomes" do
+    test "GIVEN: Forms in previous pages have been filled, WHEN: Researcher tries to add two job steps, THEN: Two job step fields should be on the page", %{conn: conn} do
+      {:ok, view, _html} = live(conn, ~p"/odi-survey/create")
+      submit_landing_page_form(view)
+      submit_market_definition_form(view)
 
-  #     resulting_html = add_two_job_steps(view)
+      resulting_html = add_two_job_steps(view)
 
-  #     assert resulting_html =~ "Job Step 1"
-  #     assert has_element?(view, ~s{input[name="form[job_steps][0][name]"]})
-  #     assert resulting_html =~ "Job Step 2"
-  #     assert has_element?(view, ~s{input[name="form[job_steps][1][name]"]})
-  #   end
+      assert resulting_html =~ "Job Step 1"
+      assert has_element?(view, ~s{input[name="form[job_steps][0][name]"]})
+      assert resulting_html =~ "Job Step 2"
+      assert has_element?(view, ~s{input[name="form[job_steps][1][name]"]})
+    end
 
-  #   test "GIVEN: Two job steps have been added, WHEN: Researcher tries to add two desired outcomes each to both job steps, THEN: Two desired outcome fields are added to the first job step", %{conn: conn} do
-  #     {:ok, view, _html} = live(conn, ~p"/odi-survey/create")
-  #     submit_landing_page_form(view)
-  #     submit_market_definition_form(view)
-  #     add_two_job_steps(view)
+    test "GIVEN: Two job steps have been added, WHEN: Researcher tries to add two desired outcomes each to both job steps, THEN: Two desired outcome fields are added to the first job step", %{conn: conn} do
+      {:ok, view, _html} = live(conn, ~p"/odi-survey/create")
+      submit_landing_page_form(view)
+      submit_market_definition_form(view)
+      add_two_job_steps(view)
 
-  #     _resulting_html = add_two_desired_outcomes_each(view)
+      _resulting_html = add_two_desired_outcomes_each(view)
 
-  #     assert has_element?(view, ~s{input[name="form[job_steps][0][desired_outcomes][0][value]"]})
-  #     assert has_element?(view, ~s{input[name="form[job_steps][0][desired_outcomes][1][value]"]})
-  #     assert has_element?(view, ~s{input[name="form[job_steps][1][desired_outcomes][0][value]"]})
-  #     assert has_element?(view, ~s{input[name="form[job_steps][1][desired_outcomes][1][value]"]})
-  #   end
+      assert has_element?(view, ~s{input[name="form[job_steps][0][desired_outcomes][0][value]"]})
+      assert has_element?(view, ~s{input[name="form[job_steps][0][desired_outcomes][1][value]"]})
+      assert has_element?(view, ~s{input[name="form[job_steps][1][desired_outcomes][0][value]"]})
+      assert has_element?(view, ~s{input[name="form[job_steps][1][desired_outcomes][1][value]"]})
+    end
 
-  #   test "GIVEN: Two desired outcome fields each have been added to two job steps, WHEN: Researcher tries to submit the desired outceoms, THEN: The screening question page is displayed", %{conn: conn} do
-  #     {:ok, view, _html} = live(conn, ~p"/odi-survey/create")
-  #     submit_landing_page_form(view)
-  #     submit_market_definition_form(view)
-  #     add_two_job_steps(view)
-  #     add_two_desired_outcomes_each(view)
+    test "GIVEN: Two desired outcome fields each have been added to two job steps, WHEN: Researcher tries to submit the desired outceoms, THEN: The screening question page is displayed", %{conn: conn} do
+      {:ok, view, _html} = live(conn, ~p"/odi-survey/create")
+      submit_landing_page_form(view)
+      submit_market_definition_form(view)
+      add_two_job_steps(view)
+      add_two_desired_outcomes_each(view)
 
-  #     _resulting_html = submit_desired_outcomes_form(view)
+      _resulting_html = submit_desired_outcomes_form(view)
 
-  #     _market_definition_page_path = assert_patch(view)
-  #     _desired_outcomes_page_path = assert_patch(view)
-  #     path = assert_patch(view)
-  #     assert path =~ ~p"/odi-survey/create/screening-questions"
-  #     assert has_element?(view, "h2", "Screening Questions")
-  #     assert_landing_page_query_params(path)
-  #     assert_market_definition_page_query_params(path)
-  #     assert_desired_outcomes_page_query_params(path)
-  #   end
-  # end
+      _market_definition_page_path = assert_patch(view)
+      _desired_outcomes_page_path = assert_patch(view)
+      path = assert_patch(view)
+      assert path =~ ~p"/odi-survey/create/screening-questions"
+      assert has_element?(view, "h2", "Screening Questions")
+      assert_landing_page_query_params(path)
+      assert_market_definition_page_query_params(path)
+      assert_desired_outcomes_page_query_params(path)
+    end
+  end
 
   # describe "Screening Questions" do
   #   test "GIVEN: Forms in previous pages have been filled, WHEN: Researcher tries to add two screening questions, THEN: Two screening question fields are added", %{conn: conn} do
