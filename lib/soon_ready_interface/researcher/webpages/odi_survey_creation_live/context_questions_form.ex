495978f4dbf2456d8991e1e33097e740cc341220
KingdomCoder <kingdomcoding@gmail.com>
2024-07-15 14:00:09 +0100

    feat: add view model for mcq and checkbox questions

--- lib/soon_ready_interface/researcher/webpages/odi_survey_creation_live/context_questions_form.ex
+++ lib/soon_ready_interface/researcher/webpages/odi_survey_creation_live/context_questions_form.ex
@@ -3,7 +3,7 @@ defmodule SoonReadyInterface.Researcher.Webpages.OdiSurveyCreationLive.ContextQu
   use Ash.Resource, data_layer: :embedded
   import SoonReadyInterface.Researcher.Webpages.OdiSurveyCreationLive.Components.Form
 
-  alias __MODULE__.ContextQuestionField
+  alias __MODULE__.{ContextQuestionField, MultipleChoiceQuestion, CheckboxQuestion}
 
   attributes do
     attribute :context_questions, {:array, ContextQuestionField}, allow_nil?: false, public?: true
@@ -15,30 +15,67 @@ defmodule SoonReadyInterface.Researcher.Webpages.OdiSurveyCreationLive.ContextQu
     <div>
       <.card_form :let={f} for={@form} phx-change="validate" phx-submit="submit" target={@myself}>
         <.inputs_for :let={ff} field={f[:context_questions]}>
-          <.card>
-            <:header>
-              <.card_header>
-                <:title>Prompt</:title>
-                <:thrash_button click="remove-context-question" name={"#{ff.name}"} target={@myself}>Remove Context Question</:thrash_button>
-                <:text_input field={ff[:prompt]} placeholder="Enter prompt" />
-              </.card_header>
-            </:header>
-            <:body>
-              <.inputs_for :let={fff} field={ff[:options]}>
-                <.card_field>
-                  <:text_input field={fff[:value]} placeholder="Option" />
-                  <:thrash_button click="remove-context-question-option" name={"#{fff.name}"} target={@myself}>Remove Context Question Option</:thrash_button>
-                </.card_field>
-              </.inputs_for>
-            </:body>
-            <:add_button name={ff.name} action="add-context-question-option" target={@myself} field={ff[:options]}> Add option </:add_button>
-          </.card>
+          <%= case ff.source.resource do %>
+            <% MultipleChoiceQuestion -> %>
+              <.mcq_card form={ff} target={@myself} />
+            <% CheckboxQuestion -> %>
+              <.checkbox_card form={ff} target={@myself} />
+          <% end %>
         </.inputs_for>
 
-        <:add_button action="add-context-question" form_field={:context_questions}> Add context question </:add_button>
+        <div class="flex flex-col gap-4">
+          <.add_button name="add-multiple-choice-question" target={@myself} action="add-multiple-choice-question" field={f[:context_questions]}> Add Multiple Choice Question </.add_button>
+          <.add_button name="add-checkbox-question" target={@myself} action="add-checkbox-question" field={f[:context_questions]}> Add Checkbox Question </.add_button>
+        </div>
         <:submit>Proceed</:submit>
       </.card_form>
     </div>
+    """
+  end
+
+  def mcq_card(assigns) do
+    ~H"""
+    <.card>
+      <:header>
+        <.card_header>
+          <:title>MCQ Prompt</:title>
+          <:thrash_button click="remove-context-question" name={"#{@form.name}"} target={@target}>Remove Context Question</:thrash_button>
+          <:text_input field={@form[:prompt]} placeholder="Enter prompt" />
+        </.card_header>
+      </:header>
+      <:body>
+        <.inputs_for :let={f} field={@form[:options]}>
+          <.card_field>
+            <:text_input field={f[:value]} placeholder="Option" />
+            <:thrash_button click="remove-context-question-option" name={"#{f.name}"} target={@target}>Remove Context Question Option</:thrash_button>
+          </.card_field>
+        </.inputs_for>
+      </:body>
+      <:add_button name={@form.name} action="add-context-question-option" target={@target} field={@form[:options]}> Add option </:add_button>
+    </.card>
+    """
+  end
+
+  def checkbox_card(assigns) do
+    ~H"""
+    <.card>
+      <:header>
+        <.card_header>
+          <:title>Checkbox Prompt</:title>
+          <:thrash_button click="remove-context-question" name={"#{@form.name}"} target={@target}>Remove Context Question</:thrash_button>
+          <:text_input field={@form[:prompt]} placeholder="Enter prompt" />
+        </.card_header>
+      </:header>
+      <:body>
+        <.inputs_for :let={f} field={@form[:options]}>
+          <.card_field>
+            <:text_input field={f[:value]} placeholder="Option" />
+            <:thrash_button click="remove-context-question-option" name={"#{f.name}"} target={@target}>Remove Context Question Option</:thrash_button>
+          </.card_field>
+        </.inputs_for>
+      </:body>
+      <:add_button name={@form.name} action="add-context-question-option" target={@target} field={@form[:options]}> Add option </:add_button>
+    </.card>
     """
   end
 
@@ -72,8 +109,13 @@ defmodule SoonReadyInterface.Researcher.Webpages.OdiSurveyCreationLive.ContextQu
   end
 
   @impl true
-  def handle_event("add-context-question", _params, socket) do
-    {:noreply, assign(socket, form: AshPhoenix.Form.add_form(socket.assigns.form, :context_questions, validate?: socket.assigns.form.errors || false))}
+  def handle_event("add-multiple-choice-question", _params, socket) do
+    {:noreply, assign(socket, form: AshPhoenix.Form.add_form(socket.assigns.form, :context_questions, params: %{"_union_type" => "#{MultipleChoiceQuestion}"}, validate?: socket.assigns.form.errors || false))}
+  end
+
+  @impl true
+  def handle_event("add-checkbox-question", _params, socket) do
+    {:noreply, assign(socket, form: AshPhoenix.Form.add_form(socket.assigns.form, :context_questions, params: %{"_union_type" => "#{CheckboxQuestion}"}, validate?: socket.assigns.form.errors || false))}
   end
 
   @impl true
--- lib/soon_ready_interface/researcher/webpages/odi_survey_creation_live/context_questions_form/context_question_field.ex
+++ lib/soon_ready_interface/researcher/webpages/odi_survey_creation_live/context_questions_form/context_question_field.ex
@@ -1,10 +1,11 @@
 defmodule SoonReadyInterface.Researcher.Webpages.OdiSurveyCreationLive.ContextQuestionsForm.ContextQuestionField do
-  use Ash.Resource, data_layer: :embedded
+  alias SoonReadyInterface.Researcher.Webpages.OdiSurveyCreationLive.ContextQuestionsForm.{
+    MultipleChoiceQuestion,
+    CheckboxQuestion,
+  }
 
-  alias SoonReadyInterface.Researcher.Webpages.OdiSurveyCreationLive.ContextQuestionsForm.OptionField
-
-  attributes do
-    attribute :prompt, :string, allow_nil?: false, public?: true
-    attribute :options, {:array, OptionField}, allow_nil?: false, public?: true, constraints: [min_length: 2]
-  end
+  use Ash.Type.NewType, subtype_of: :union, constraints: [types: [
+    {MultipleChoiceQuestion, [type: MultipleChoiceQuestion, tag: :type, tag_value: "multiple_choice_question"]},
+    {CheckboxQuestion, [type: CheckboxQuestion, tag: :type, tag_value: "checkbox_question"]},
+  ]]
 end
