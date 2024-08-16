d1290123e72de4a5030d33302a899c66f272955f
KingdomCoder <kingdomcoding@gmail.com>
2024-08-16 15:21:22 +0100

    refactored: update event to use dumped data

--- lib/soon_ready/survey_management/v1/domain_events/survey_created.ex
+++ lib/soon_ready/survey_management/v1/domain_events/survey_created.ex
@@ -4,25 +4,42 @@ defmodule SoonReady.SurveyManagement.V1.DomainEvents.SurveyCreated do
     extensions: [SoonReady.Ash.Extensions.JsonEncoder]
 
   alias SoonReady.SurveyManagement.V1.DomainConcepts.{SurveyPage, Trigger}
+  alias SoonReady.SurveyManagement.V1.DomainEvents.Helpers
 
   attributes do
     attribute :survey_id, :uuid, allow_nil?: false, primary_key?: true, public?: true
     attribute :starting_page_id, :uuid, allow_nil?: false, public?: true
-    attribute :pages, {:array, SurveyPage}, public?: true
+    attribute :pages_dumped_data, {:array, :map}, public?: true
     attribute :trigger, Trigger, public?: true
+  end
+
+  calculations do
+    calculate :pages, {:array, SurveyPage}, fn [record], _context ->
+      {:ok, pages} = Helpers.cast_stored(record.pages_dumped_data)
+      {:ok, [pages]}
+    end
+  end
+
+  changes do
+    change load(:pages)
   end
 
   actions do
     default_accept [
       :survey_id,
       :starting_page_id,
-      :pages,
+      :pages_dumped_data,
       :trigger,
     ]
+    defaults [:read]
+
     create :new
+
+    update :regenerate
   end
 
   code_interface do
     define :new
+    define :regenerate
   end
 end
