0c0718a154cdc7af6c9b79b698f8fa0732dc0fb1
KingdomCoder <kingdomcoding@gmail.com>
2024-08-16 15:09:17 +0100

    refactor: move cast stored to helper module

--- lib/soon_ready/survey_management/v1/domain_events/survey_published.ex
+++ lib/soon_ready/survey_management/v1/domain_events/survey_published.ex
@@ -4,6 +4,7 @@ defmodule SoonReady.SurveyManagement.V1.DomainEvents.SurveyPublished do
     extensions: [SoonReady.Ash.Extensions.JsonEncoder]
 
   alias SoonReady.SurveyManagement.V1.DomainConcepts.{SurveyPage, Trigger}
+  alias SoonReady.SurveyManagement.V1.DomainEvents.Helpers
 
   attributes do
     attribute :survey_id, :uuid, allow_nil?: false, primary_key?: true, public?: true
@@ -14,8 +15,7 @@ defmodule SoonReady.SurveyManagement.V1.DomainEvents.SurveyPublished do
 
   calculations do
     calculate :pages, {:array, SurveyPage}, fn [record], _context ->
-      pages_dumped_data = Enum.map(record.pages_dumped_data, &normalize/1)
-      {:ok, pages} = Ash.Type.cast_stored({:array, SurveyPage}, pages_dumped_data, [])
+      {:ok, pages} = Helpers.cast_stored(record.pages_dumped_data)
       {:ok, [pages]}
     end
   end
@@ -41,33 +41,5 @@ defmodule SoonReady.SurveyManagement.V1.DomainEvents.SurveyPublished do
   code_interface do
     define :new
     define :regenerate
-  end
-
-  def normalize(map) do
-    map
-    |> Enum.map(&do_normalize/1)
-    |> Enum.into(%{})
-  end
-
-  defp do_normalize({key, value}) when key in ["type", :type] do
-    value =
-      if is_atom(value) do
-        value
-      else
-        String.to_existing_atom(value)
-      end
-    {to_string(key), value}
-  end
-
-  defp do_normalize({key, value}) when is_map(value) do
-    {to_string(key), normalize(value)}
-  end
-
-  defp do_normalize({key, value}) when is_list(value) do
-    {to_string(key), Enum.map(value, &normalize/1)}
-  end
-
-  defp do_normalize({key, value}) do
-    {to_string(key), value}
   end
 end
