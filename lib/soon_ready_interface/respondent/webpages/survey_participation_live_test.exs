8e6f38e476f342dbccfaf3c88bc6e117c615df37
KingdomCoder <kingdomcoding@gmail.com>
2024-08-22 13:25:59 +0100

    refactor: clean code

--- lib/soon_ready_interface/respondent/webpages/survey_participation_live_test.exs
+++ lib/soon_ready_interface/respondent/webpages/survey_participation_live_test.exs
@@ -4,6 +4,58 @@ defmodule SoonReadyInterface.Respondent.Webpages.SurveyParticipationLiveTest do
 
   alias SoonReadyInterface.Respondent.ReadModels.Survey
 
+
+  # FORM PARAMS
+
+  @nickname_form_params %{
+    "responses" => %{"0" => %{"response" => "A Nickname"}},
+  }
+
+  @correct_screening_form_params %{
+    "responses" => %{
+      "0" => %{"response" => "Option 1"},
+      "1" => %{"response" => "Option 1"}
+    }
+  }
+
+  @incorrect_screening_form_params %{
+    "responses" => %{
+      "0" => %{"response" => "Option 1"},
+      "1" => %{"response" => "Option 2"}
+    }
+  }
+
+  @contact_details_form_params %{
+    "responses" => %{
+      "0" => %{"response" => "hello@example.com"},
+      "1" => %{"response" => "1234567890"}
+    }
+  }
+
+  @demographics_form_params %{
+    "responses" => %{
+      "0" => %{"response" => "Option 1"},
+      "1" => %{"response" => "Option 1"}
+    }
+  }
+
+  @context_form_params %{
+    "responses" => %{
+      "0" => %{"response" => "Option 1"},
+      "1" => %{"response" => "Option 1"}
+    }
+  }
+
+  @comparison_form_params %{
+    "responses" => %{
+      "0" => %{"response" => "Product 1, Service 2, Platform 3"},
+      "1" => %{"response" => "Thing 1, Thing 2"},
+      "2" => %{"response" => "1000"},
+      "3" => %{"response" => "Yes"},
+      "4" => %{"response" => "1000"},
+    }
+  }
+
   @desired_outcome_form_params %{
     "responses" => %{
       "0" => %{
@@ -21,53 +73,14 @@ defmodule SoonReadyInterface.Respondent.Webpages.SurveyParticipationLiveTest do
     }
   }
 
-  @comparison_page_query_params %{
-    "0" => %{"prompt" => "What products, services or platforms have you used to do what persons do?", "response" => "Product 1, Service 2, Platform 3"},
-    "1" => %{"prompt" => "What additional things do you usually use/require when you're using any of the above?", "response" => "Thing 1, Thing 2"},
-    "2" => %{"prompt" => "In total, how much would you estimate that you spend annually to do what persons do?", "response" => "1000"},
-    "3" => %{"prompt" => "Would you be willing to pay more for a better solution?", "response" => "Yes"},
-    "4" => %{"prompt" => "If yes, how much extra would you be willing to pay annually to get the job done perfectly?", "response" => "1000"}
-  }
 
-  @context_form_params %{
-    "responses" => %{
-      "0" => %{"response" => "Option 1"},
-      "1" => %{"response" => "Option 1"}
-    }
-  }
+  # QUERY PARAMS
 
-  @context_page_query_params %{
-    "0" => %{"prompt" => "What is the answer to context question 1?", "response" => "Option 1"},
-    "1" => %{"prompt" => "What is the answer to context question 2?", "response" => "Option 1"}
-  }
+  @landing_page_query_params %{"nickname" => "A Nickname"}
 
-  @demographics_form_params %{
-    "responses" => %{
-      "0" => %{"response" => "Option 1"},
-      "1" => %{"response" => "Option 1"}
-    }
-  }
-
-  @demographics_page_query_params %{
-    "0" => %{"prompt" => "What is the answer to demographic question 1?", "response" => "Option 1"},
-    "1" => %{"prompt" => "What is the answer to demographic question 2?", "response" => "Option 1"}
-  }
-
-  @contact_details_form_params %{
-    "responses" => %{
-      "0" => %{"response" => "hello@example.com"},
-      "1" => %{"response" => "1234567890"}
-    }
-  }
-
-  @comparison_form_params %{
-    "responses" => %{
-      "0" => %{"response" => "Product 1, Service 2, Platform 3"},
-      "1" => %{"response" => "Thing 1, Thing 2"},
-      "2" => %{"response" => "1000"},
-      "3" => %{"response" => "Yes"},
-      "4" => %{"response" => "1000"},
-    }
+  @screening_page_query_params %{
+    "0" => %{"prompt" => "What is the answer to screening question 1?", "response" => "Option 1"},
+    "1" => %{"prompt" => "What is the answer to screening question 2?", "response" => "Option 1"}
   }
 
   @contact_details_page_query_params %{
@@ -75,30 +88,23 @@ defmodule SoonReadyInterface.Respondent.Webpages.SurveyParticipationLiveTest do
     "phone_number" => "1234567890"
   }
 
-  @correct_screening_form_params %{
-    "responses" => %{
-      "0" => %{"response" => "Option 1"},
-      "1" => %{"response" => "Option 1"}
-    }
+  @demographics_page_query_params %{
+    "0" => %{"prompt" => "What is the answer to demographic question 1?", "response" => "Option 1"},
+    "1" => %{"prompt" => "What is the answer to demographic question 2?", "response" => "Option 1"}
   }
 
-  @incorrect_screening_form_params %{
-    "responses" => %{
-      "0" => %{"response" => "Option 1"},
-      "1" => %{"response" => "Option 2"}
-    }
+  @context_page_query_params %{
+    "0" => %{"prompt" => "What is the answer to context question 1?", "response" => "Option 1"},
+    "1" => %{"prompt" => "What is the answer to context question 2?", "response" => "Option 1"}
   }
 
-  @screening_page_query_params %{
-    "0" => %{"prompt" => "What is the answer to screening question 1?", "response" => "Option 1"},
-    "1" => %{"prompt" => "What is the answer to screening question 2?", "response" => "Option 1"}
+  @comparison_page_query_params %{
+    "0" => %{"prompt" => "What products, services or platforms have you used to do what persons do?", "response" => "Product 1, Service 2, Platform 3"},
+    "1" => %{"prompt" => "What additional things do you usually use/require when you're using any of the above?", "response" => "Thing 1, Thing 2"},
+    "2" => %{"prompt" => "In total, how much would you estimate that you spend annually to do what persons do?", "response" => "1000"},
+    "3" => %{"prompt" => "Would you be willing to pay more for a better solution?", "response" => "Yes"},
+    "4" => %{"prompt" => "If yes, how much extra would you be willing to pay annually to get the job done perfectly?", "response" => "1000"}
   }
-
-  @nickname_form_params %{
-    "responses" => %{"0" => %{"response" => "A Nickname"}},
-  }
-
-  @landing_page_query_params %{"nickname" => "A Nickname"}
 
   setup do
     params = %{
